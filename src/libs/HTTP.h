cba5e27089dc754056377ee46847db4ef5bc6ee5
jimmyjordanSWE <169525180+jimmyKodar@users.noreply.github.com>
2025-09-18 12:12:52 +0200

    fiexed // comments removed utils.h

--- src/libs/HTTP.h
+++ src/libs/HTTP.h
@@ -1,17 +1,14 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef struct
-{
-	char* data;
-    size_t size;
+typedef struct {
+  char *data;
+  size_t size;
 
 } HTTP;
 
+int HTTP_init(HTTP *http);
 
-int HTTP_init(HTTP* http);
+int HTTP_get(HTTP *http, const char *_URL);
 
-int HTTP_get(HTTP* http, const char* _URL);
-
-void HTTP_dispose(HTTP* http);
-
+void HTTP_dispose(HTTP *http);
