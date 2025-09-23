82d1906331b0e3d47c56da97dffaf1cbf3e4c2b4
Patrik Johansson <Patrik.Viby@gmail.com>
2025-09-23 15:28:58 +0200

    Changed weather->city. Added functions for parsing, creating and disposing city structs

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
+#include "city.h"
 #include "curl_helper.h"
 #include "jansson_helper.h"
 #include "libs/jansson/jansson.h"
-#include "weather.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,6 +37,8 @@ int main() {
   if (!curl_init(&curl)) {
     return 1; /* error print is handled in curl_init */
   }
+
+  City_parse_cities(NULL, (char *)cities);
 
   printf("Välkommen!\n\n");
 
