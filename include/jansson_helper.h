dd0f89e336d37bafee1654ee25fa202147ddb2a4
Gustav Frisén <gustav.frisen@outlook.com>
2025-09-23 15:27:17 +0200

    jansson rework v.1.02

--- include/jansson_helper.h
+++ include/jansson_helper.h
@@ -2,7 +2,18 @@
 
 int check_weather_cache(char* cityName);
 void print_temperature(char* cityName);
+/**/
+typedef struct {
+    char time[20];
+    int interval;
+    double temperature;
+    double windspeed;
+    int winddirection;
+    int is_day;
+    int weathercode;
+} current_weather;
 
 int does_json_exist(char* cityName);
 int is_weather_old(char* cityName);
 int print_weather(char* cityName);
+current_weather get_weather();
--- src/libs/jansson_helper.c
+++ src/libs/jansson_helper.c
@@ -1,6 +1,8 @@
 #include "jansson.h"
 #include <time.h>
-
+#include <jansson.h>
+#include "jansson_helper.h"
+/**/
 int check_weather_cache(char *cityName) {
   /* Check local cache first */
   char cityFile[55];
@@ -182,4 +184,60 @@ int print_weather(char *cityName) {
 
   json_decref(root);
   return 0;
+}
+
+current_weather get_weather(char* cityName)
+{
+    current_weather cw = {0};  // initialize all fields to safe defaults
+
+    char cityFile[55];
+    snprintf(cityFile, sizeof(cityFile), "cache/%s.json", cityName);
+
+    json_error_t error;
+    json_t *root = json_load_file(cityFile, 0, &error);
+    if (!root) {
+        fprintf(stderr, "Error loading JSON: %s (line %d, col %d)\n",
+                error.text, error.line, error.column);
+        return cw;  // return empty struct on error
+    }
+
+    json_t *current_weather = json_object_get(root, "current_weather");
+    if (!json_is_object(current_weather)) {
+        json_decref(root);
+        return cw;  // return empty struct if missing
+    }
+
+    // Extract values
+    json_t *time_val        = json_object_get(current_weather, "time");
+    json_t *interval_val    = json_object_get(current_weather, "interval");
+    json_t *temp_val        = json_object_get(current_weather, "temperature");
+    json_t *wind_val        = json_object_get(current_weather, "windspeed");
+    json_t *winddir_val     = json_object_get(current_weather, "winddirection");
+    json_t *isday_val       = json_object_get(current_weather, "is_day");
+    json_t *wcode_val       = json_object_get(current_weather, "weathercode");
+
+    if (json_is_string(time_val)) {
+        strncpy(cw.time, json_string_value(time_val), sizeof(cw.time) - 1);
+    }
+    if (json_is_integer(interval_val)) {
+        cw.interval = (int)json_integer_value(interval_val);
+    }
+    if (json_is_number(temp_val)) {
+        cw.temperature = json_number_value(temp_val);
+    }
+    if (json_is_number(wind_val)) {
+        cw.windspeed = json_number_value(wind_val);
+    }
+    if (json_is_integer(winddir_val)) {
+        cw.winddirection = (int)json_integer_value(winddir_val);
+    }
+    if (json_is_integer(isday_val)) {
+        cw.is_day = (int)json_integer_value(isday_val);
+    }
+    if (json_is_integer(wcode_val)) {
+        cw.weathercode = (int)json_integer_value(wcode_val);
+    }
+
+    json_decref(root);
+    return cw;
 }
