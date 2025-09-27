3d61ea7c1e81d39838c0249cda3885337c02acb8
Patrik Johansson <Patrik.Viby@gmail.com>
2025-09-27 16:57:26 +0200

    Added functionality to free store items in ll_clear and ll_dispose + formatting

--- include/linked_list.h
+++ include/linked_list.h
@@ -4,28 +4,41 @@
 #include <stddef.h>
 
 typedef struct Node Node;
-struct Node
-{
-    Node *back;
-    Node *front;
-    void *item;
+struct Node {
+  Node *back;
+  Node *front;
+  void *item;
 };
 
-typedef struct
-{
-    Node *head;
-    Node *tail;
-    size_t size;
+typedef struct {
+  Node *head;
+  Node *tail;
+  size_t size;
 } LinkedList;
 
-LinkedList *LinkedList_create(); /* initialize new LinkedList */
-Node* LinkedList_get_index(LinkedList *list, size_t index); /* return Node at specific index in LinkedList */
-int LinkedList_insert(LinkedList *list, size_t index, void *item); /* add void pointer to specified index of LinkedList */
-int LinkedList_append(LinkedList *list, void *item); /* add void pointer to end of LinkedList */
-int LinkedList_remove(LinkedList *list, Node *item); /* remove Node from LinkedList by reference */
-int LinkedList_pop(LinkedList *list, size_t index); /* remove Node from LinkedList at index */
+/* initialize new LinkedList */
+LinkedList *LinkedList_create();
 
-void LinkedList_clear(LinkedList *list); /* remove all nodes in a LinkedList */
-void LinkedList_dispose(LinkedList *list); /* dispose LinkedList, does NOT free memory of items in nodes as we cannot ensure it's safe */
+/* return Node at specific index in LinkedList */
+Node *LinkedList_get_index(LinkedList *list, size_t index);
+
+/* add void pointer to specified index of LinkedList */
+int LinkedList_insert(LinkedList *list, size_t index, void *item);
+
+/* add void pointer to end of LinkedList */
+int LinkedList_append(LinkedList *list, void *item);
+
+/* remove Node from LinkedList by reference */
+int LinkedList_remove(LinkedList *list, Node *item);
+
+/* remove Node from LinkedList at index */
+int LinkedList_pop(LinkedList *list, size_t index);
+
+/* remove all nodes in a LinkedList */
+void LinkedList_clear(LinkedList *list, void free_function(void *));
+
+/* dispose LinkedList, does NOT free memory of items in nodes
+                 as we cannot ensure it's safe */
+void LinkedList_dispose(LinkedList *list, void free_function(void *));
 
 #endif
