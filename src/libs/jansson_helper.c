3ee32913674ec5ae4afcc64e2618f6ec9c494287
Gustav Frisén <gustav.frisen@outlook.com>
2025-10-06 20:57:18 +0200

    jansson_helper.h/c documentation added.

--- src/libs/jansson_helper.c
+++ src/libs/jansson_helper.c
@@ -62,40 +62,6 @@ int check_weather_cache(char *file_name) {
   return 0;
 }
 
-void print_temperature(char *cityName) {
-  /* Check local cache first */
-  char cityFile[55];
-  snprintf(cityFile, sizeof(cityFile), "cache/%s.json", cityName);
-
-  /*  Finns staden lokalt? */
-  json_error_t error;
-  json_t *root = json_load_file(cityFile, 0, &error);
-  if (!root) {
-    fprintf(stderr, "Error loading JSON: %s (line %d, col %d)\n", error.text,
-            error.line, error.column);
-    return; /* Staden finns inte lokalt */
-  }
-
-  /* Staden finns lokalt, är den gammal? */
-  json_t *current_weather = json_object_get(root, "current_weather");
-  if (!json_is_object(current_weather)) {
-    json_decref(root);
-    return;
-  }
-
-  json_t *temp_val = json_object_get(current_weather, "temperature");
-  json_t *wind_val = json_object_get(current_weather, "windspeed");
-
-  if (json_is_number(temp_val) && json_is_number(wind_val)) {
-    double temp = json_number_value(temp_val);
-    double wind = json_number_value(wind_val);
-
-    printf("\n  Temperatur: %.1f °C\n", temp);
-    printf("  Vind: %.1f km/h\n", wind);
-  }
-  json_decref(root);
-}
-
 int does_json_exist(char *cityName) {
   char cityFile[55];
   snprintf(cityFile, sizeof(cityFile), "cache/%s.json", cityName);
@@ -108,52 +74,6 @@ int does_json_exist(char *cityName) {
 
   json_decref(root);
   return 0; /* Staden finns lokalt */
-}
-
-int is_weather_old(char *cityName) {
-  char cityFile[55];
-  snprintf(cityFile, sizeof(cityFile), "cache/%s.json", cityName);
-
-  json_error_t error;
-  json_t *root = json_load_file(cityFile, 0, &error);
-
-  json_t *current_weather = json_object_get(root, "current_weather");
-  if (!json_is_object(current_weather)) {
-    json_decref(root);
-    return -1;
-  }
-
-  json_t *time_val = json_object_get(current_weather, "time");
-  if (!json_is_string(time_val)) {
-    json_decref(root);
-    return -1;
-  }
-  const char *time_str = json_string_value(time_val);
-
-  struct tm tm_time = {0};
-  if (strptime(time_str, "%Y-%m-%dT%H:%M", &tm_time) == 0) {
-    fprintf(stderr, "Failed to parse time string: %s\n", time_str);
-    json_decref(root);
-    return -1;
-  }
-
-  time_t weather_time = timegm(&tm_time);
-  time_t now = time(NULL);
-
-  json_t *interval_val = json_object_get(current_weather, "interval");
-  int interval = (json_is_integer(interval_val))
-                     ? (int)json_integer_value(interval_val)
-                     : 900;
-
-  double diff = difftime(now, weather_time);
-
-  if (diff > interval) {
-    json_decref(root);
-    return 0; /* Vädret är gammalt */
-  }
-
-  json_decref(root);
-  return 1; /* Vädret är inte gammalt */
 }
 
 int print_weather2(char *cityName) {
@@ -255,7 +175,7 @@ int read_cities_from_file(char *file_name, LinkedList *list) {
 
   if (!json_is_array(file)) {
     json_decref(file);
-    return -1;
+    return 1;
   }
   for (int i = 0; i < (int)json_array_size(file); i++) {
     json_t *item = json_array_get(file, i);
@@ -265,15 +185,15 @@ int read_cities_from_file(char *file_name, LinkedList *list) {
 
     if (!json_is_string(name)) {
       json_decref(file);
-      return -1;
+      return 1;
     }
     if (!json_is_real(latitude)) {
       json_decref(file);
-      return -1;
+      return 1;
     }
     if (!json_is_real(longitude)) {
       json_decref(file);
-      return -1;
+      return 1;
     }
 
     city *new_city =
